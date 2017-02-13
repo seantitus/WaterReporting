6dcc30281260cce89a45d4bc86aa865c358b0fb6
nancytao <nancy.tao42@gmail.com>
2017-02-13 10:32:29 -0500

    Added logout button

--- app/src/main/java/com/zoinks/waterreporting/MainActivity.java
+++ app/src/main/java/com/zoinks/waterreporting/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        Button logout = (Button) findViewById(R.id.logout_button);
+        logout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                finish();
             }
         });
     }
