6ae181e6e019bf35913c93690b61fe4ebcdc8553
nancytao <nancy.tao42@gmail.com>
2017-03-30 13:57:52 -0400

    Added latitude and longitude on historical graph

--- app/src/main/java/com/zoinks/waterreporting/controllers/HistoricalReportOptionsActivity.java
+++ app/src/main/java/com/zoinks/waterreporting/controllers/HistoricalReportOptionsActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +15,8 @@ import com.zoinks.waterreporting.R;
 
 public class HistoricalReportOptionsActivity extends AppCompatActivity {
     private TextView mYearView;
+    private TextView mLongitudeView;
+    private TextView mLatitudeView;
     private RadioGroup mRadioGroup;
 
     @Override
@@ -24,6 +25,8 @@ public class HistoricalReportOptionsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_historical_report_options);
 
         mYearView = (TextView) findViewById(R.id.year);
+        mLongitudeView = (EditText) findViewById(R.id.longitude);
+        mLatitudeView = (EditText) findViewById(R.id.latitude);
         mRadioGroup = (RadioGroup) findViewById(R.id.radio);
 
         Button mSubmitButton = (Button) findViewById(R.id.submit_button);
@@ -42,7 +45,6 @@ public class HistoricalReportOptionsActivity extends AppCompatActivity {
                 finish();
             }
         });
-
     }
 
     /**
@@ -50,11 +52,19 @@ public class HistoricalReportOptionsActivity extends AppCompatActivity {
      */
     private void attemptSubmission() {
         String yearString = mYearView.getText().toString();
+        String latString = mLatitudeView.getText().toString();
+        String longString = mLongitudeView.getText().toString();
 
-        // check that users input a year, otherwise tell them it's required
+        // check that users input stuff, otherwise tell them it's required
         if (TextUtils.isEmpty(yearString)) {
             mYearView.setError(getString(R.string.error_field_required));
             mYearView.requestFocus();
+        } else if (TextUtils.isEmpty(latString)) {
+            mLatitudeView.setError(getString(R.string.error_field_required));
+            mLatitudeView.requestFocus();
+        } else if (TextUtils.isEmpty(longString)) {
+            mLongitudeView.setError(getString(R.string.error_field_required));
+            mLongitudeView.requestFocus();
         } else {
             // check that the latitude and longitude are valid
             double latitude = Double.parseDouble(latString);
