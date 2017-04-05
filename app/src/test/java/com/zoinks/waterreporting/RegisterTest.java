aca9a34c6911f8e63e78f65032cf7329ad63cadd
vvinutna <vinutna.veeragandham@gmail.com>
2017-04-05 18:50:51 -0400

    update to Register tests

--- app/src/test/java/com/zoinks/waterreporting/RegisterTest.java
+++ app/src/test/java/com/zoinks/waterreporting/RegisterTest.java
@@ -1,7 +1,7 @@
 package com.zoinks.waterreporting;
 
 import com.zoinks.waterreporting.model.Facade;
-import com.zoinks.waterreporting.model.UserType
+import com.zoinks.waterreporting.model.UserType;
 
 import org.junit.Test;
 
@@ -19,37 +19,44 @@ public class RegisterTest {
         assertEquals(facade.registerUser(null, "lastName", "username", "password", UserType.USER), false);
     }
 
+    @Test
     public void nullLastName(){
         Facade facade = Facade.getInstance();
         assertEquals(facade.registerUser("firstName", null, "username", "password", UserType.USER), false);
     }
 
+    @Test
     public void nullUsername(){
         Facade facade = Facade.getInstance();
         assertEquals(facade.registerUser("firstName", "lastName", null, "password", UserType.USER), false);
     }
 
+    @Test
     public void nullPassword(){
         Facade facade = Facade.getInstance();
         assertEquals(facade.registerUser("firstName", "lastName", "username", null, UserType.USER), false);
     }
 
+    @Test
     public void nullUserType(){
         Facade facade = Facade.getInstance();
         assertEquals(facade.registerUser("firstName", "lastName", "username", "password", null), false);
     }
 
+    @Test
     public void nullAllFields(){
         Facade facade = Facade.getInstance();
         assertEquals(facade.registerUser(null, null, null, null, null), false);
     }
 
+    @Test
     public void takenUsername(){
         Facade facade = Facade.getInstance();
         facade.registerUser("firstName", "lastName", "originalUsername", "password", UserType.USER);
         assertEquals(facade.registerUser("firstName", "lastName", "originalUsername", "password", UserType.USER), false);
     }
 
+    @Test
     public void registerSuccessfully(){
         Facade facade = Facade.getInstance();
         assertEquals(facade.registerUser("firstName", "lastName", "username", "password", UserType.USER), true);
